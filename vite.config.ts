dd6001f84c64bd72ceaa238338730276f40eedaf
monster <monster@hamsterbase.com>
2023-02-10 23:59:37 +0800

    feat: update sync api

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-
-  server: {
-    proxy: {
-      '/siyuan-proxy': {
-        target: 'http://127.0.0.1:6806',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/siyuan-proxy/, ''),
-      },
-    },
-  },
 });
