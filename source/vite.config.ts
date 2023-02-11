ed5f2119d6da4ec93fc5073e7a40572d25f948e0
monster <monster@hamsterbase.com>
2023-02-11 11:19:32 +0800

    chore: move source code to source

--- source/vite.config.ts
+++ source/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: '../',
+  },
   plugins: [react()],
 });
